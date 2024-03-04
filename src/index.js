7ed4b237a7e15a0cfbcca9395fabd318c0f45d2c
Eriwith <lianxiango2@gmail.com>
2024-03-04 22:58:47 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.12y.ltd": "https://registry-1.docker.io",
+  "quay.12y.ltd": "https://quay.io",
+  "gcr.12y.ltd": "https://gcr.io",
+  "k8s-gcr.12y.ltd": "https://k8s.gcr.io",
+  "k8s.12y.ltd": "https://registry.k8s.io",
+  "ghcr.12y.ltd": "https://ghcr.io",
+  "cloudsmith.12y.ltd": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
